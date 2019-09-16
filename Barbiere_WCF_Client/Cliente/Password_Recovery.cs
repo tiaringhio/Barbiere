17794ef9d3029ae02ecc72493949b52a91996862
MattiaG98 <MattiaG98@users.noreply.github.com>
2019-09-16 09:57:01 +0200

    Added Username control into Password Recovery
    Aggiunto controllo username in PasswordRecovery.cs

--- Barbiere_WCF_Client/Cliente/Password_Recovery.cs
+++ Barbiere_WCF_Client/Cliente/Password_Recovery.cs
@@ -38,26 +38,38 @@ namespace Barbiere_WCF_Client.Cliente {
                 PasswordRecoveryNew.Focus();
                 return;
             }
-            try
+            // I check that the username exists
+            if (!WCF.UserChecker(PasswordRecoveryUser.Text))
             {
-                // The password will be hashed trough EasyEncryption, via the MD5 protocol
-                string HashedPassword = EasyEncryption.MD5.ComputeMD5Hash(PasswordRecoveryNew.Text);
-
-                // I send the data to the WCF that updates the password given the user that the client inputs.
-                WCF.PasswordRecovery(PasswordRecoveryUser.Text, HashedPassword);
-
-                // I show a message to the user letting him/her know that the password has been changed
-                MessageBox.Show("Password changed successfully, you can now login");
-
-                // Then i send the user back to the initial form to login
-                Barbiere logreg = new Barbiere();
-                this.Hide();
-                logreg.ShowDialog();
+                MessageBox.Show("Username doesn't exist!");
+                PasswordRecoveryUser.Clear();
+                PasswordRecoveryNew.Clear();
+                PasswordRecoveryNewConfirm.Clear();
+                PasswordRecoveryUser.Focus();
             }
-             catch (Exception recoveryException)
+            else
             {
-                MessageBox.Show(recoveryException.ToString());
-                throw;
+                try
+                {
+                    // The password will be hashed trough EasyEncryption, via the MD5 protocol
+                    string HashedPassword = EasyEncryption.MD5.ComputeMD5Hash(PasswordRecoveryNew.Text);
+
+                    // I send the data to the WCF that updates the password given the user that the client inputs.
+                    WCF.PasswordRecovery(PasswordRecoveryUser.Text, HashedPassword);
+
+                    // I show a message to the user letting him/her know that the password has been changed
+                    MessageBox.Show("Password changed successfully, you can now login");
+
+                    // Then i send the user back to the initial form to login
+                    Barbiere logreg = new Barbiere();
+                    this.Hide();
+                    logreg.ShowDialog();
+                }
+                catch (Exception recoveryException)
+                {
+                    MessageBox.Show(recoveryException.ToString());
+                    throw;
+                }
             }
         }
 
@@ -67,6 +79,16 @@ namespace Barbiere_WCF_Client.Cliente {
             Barbiere logreg = new Barbiere();
             this.Hide();
             logreg.ShowDialog();
+        }
+
+        private void Password_Recovery_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Panel_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
--- Barbiere_WCF_Client/Cliente/Password_Recovery.designer.cs
+++ Barbiere_WCF_Client/Cliente/Password_Recovery.designer.cs
@@ -43,30 +43,33 @@
             // PasswordRecoveryUser
             // 
             this.PasswordRecoveryUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PasswordRecoveryUser.Location = new System.Drawing.Point(350, 102);
+            this.PasswordRecoveryUser.Location = new System.Drawing.Point(525, 159);
+            this.PasswordRecoveryUser.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.PasswordRecoveryUser.Multiline = true;
             this.PasswordRecoveryUser.Name = "PasswordRecoveryUser";
-            this.PasswordRecoveryUser.Size = new System.Drawing.Size(168, 30);
+            this.PasswordRecoveryUser.Size = new System.Drawing.Size(250, 45);
             this.PasswordRecoveryUser.TabIndex = 1;
             // 
             // PasswordRecoveryNew
             // 
             this.PasswordRecoveryNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PasswordRecoveryNew.Location = new System.Drawing.Point(350, 157);
+            this.PasswordRecoveryNew.Location = new System.Drawing.Point(525, 245);
+            this.PasswordRecoveryNew.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.PasswordRecoveryNew.Multiline = true;
             this.PasswordRecoveryNew.Name = "PasswordRecoveryNew";
             this.PasswordRecoveryNew.PasswordChar = '*';
-            this.PasswordRecoveryNew.Size = new System.Drawing.Size(168, 30);
+            this.PasswordRecoveryNew.Size = new System.Drawing.Size(250, 45);
             this.PasswordRecoveryNew.TabIndex = 2;
             // 
             // PasswordRecoveryNewConfirm
             // 
             this.PasswordRecoveryNewConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PasswordRecoveryNewConfirm.Location = new System.Drawing.Point(350, 211);
+            this.PasswordRecoveryNewConfirm.Location = new System.Drawing.Point(525, 330);
+            this.PasswordRecoveryNewConfirm.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.PasswordRecoveryNewConfirm.Multiline = true;
             this.PasswordRecoveryNewConfirm.Name = "PasswordRecoveryNewConfirm";
             this.PasswordRecoveryNewConfirm.PasswordChar = '*';
-            this.PasswordRecoveryNewConfirm.Size = new System.Drawing.Size(168, 30);
+            this.PasswordRecoveryNewConfirm.Size = new System.Drawing.Size(250, 45);
             this.PasswordRecoveryNewConfirm.TabIndex = 3;
             // 
             // User
@@ -74,9 +77,10 @@
             this.User.AutoSize = true;
             this.User.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.User.ForeColor = System.Drawing.SystemColors.Control;
-            this.User.Location = new System.Drawing.Point(246, 105);
+            this.User.Location = new System.Drawing.Point(369, 164);
+            this.User.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.User.Name = "User";
-            this.User.Size = new System.Drawing.Size(43, 20);
+            this.User.Size = new System.Drawing.Size(84, 37);
             this.User.TabIndex = 1;
             this.User.Text = "User";
             // 
@@ -85,9 +89,10 @@
             this.NewPassword.AutoSize = true;
             this.NewPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.NewPassword.ForeColor = System.Drawing.SystemColors.Control;
-            this.NewPassword.Location = new System.Drawing.Point(176, 160);
+            this.NewPassword.Location = new System.Drawing.Point(264, 250);
+            this.NewPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.NewPassword.Name = "NewPassword";
-            this.NewPassword.Size = new System.Drawing.Size(113, 20);
+            this.NewPassword.Size = new System.Drawing.Size(231, 37);
             this.NewPassword.TabIndex = 1;
             this.NewPassword.Text = "New Password";
             // 
@@ -96,9 +101,10 @@
             this.ConfirmPassword.AutoSize = true;
             this.ConfirmPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ConfirmPassword.ForeColor = System.Drawing.SystemColors.Control;
-            this.ConfirmPassword.Location = new System.Drawing.Point(152, 214);
+            this.ConfirmPassword.Location = new System.Drawing.Point(228, 334);
+            this.ConfirmPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ConfirmPassword.Name = "ConfirmPassword";
-            this.ConfirmPassword.Size = new System.Drawing.Size(137, 20);
+            this.ConfirmPassword.Size = new System.Drawing.Size(280, 37);
             this.ConfirmPassword.TabIndex = 1;
             this.ConfirmPassword.Text = "Confirm Password";
             // 
@@ -114,18 +120,21 @@
             this.Panel.Controls.Add(this.PasswordRecoveryNewConfirm);
             this.Panel.Controls.Add(this.PasswordRecoveryNew);
             this.Panel.Controls.Add(this.PasswordRecoveryUser);
-            this.Panel.Location = new System.Drawing.Point(236, 133);
+            this.Panel.Location = new System.Drawing.Point(354, 208);
+            this.Panel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Panel.Name = "Panel";
-            this.Panel.Size = new System.Drawing.Size(689, 354);
+            this.Panel.Size = new System.Drawing.Size(1034, 553);
             this.Panel.TabIndex = 2;
+            this.Panel.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel_Paint);
             // 
             // BackToLogin
             // 
             this.BackToLogin.AutoSize = true;
             this.BackToLogin.LinkColor = System.Drawing.SystemColors.Control;
-            this.BackToLogin.Location = new System.Drawing.Point(568, 167);
+            this.BackToLogin.Location = new System.Drawing.Point(852, 261);
+            this.BackToLogin.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.BackToLogin.Name = "BackToLogin";
-            this.BackToLogin.Size = new System.Drawing.Size(85, 16);
+            this.BackToLogin.Size = new System.Drawing.Size(136, 25);
             this.BackToLogin.TabIndex = 32;
             this.BackToLogin.TabStop = true;
             this.BackToLogin.Text = "Back to login";
@@ -136,9 +145,10 @@
             this.PasswordRecoveryTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(27)))), ((int)(((byte)(27)))));
             this.PasswordRecoveryTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PasswordRecoveryTitle.ForeColor = System.Drawing.SystemColors.Control;
-            this.PasswordRecoveryTitle.Location = new System.Drawing.Point(133, 38);
+            this.PasswordRecoveryTitle.Location = new System.Drawing.Point(200, 59);
+            this.PasswordRecoveryTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.PasswordRecoveryTitle.Name = "PasswordRecoveryTitle";
-            this.PasswordRecoveryTitle.Size = new System.Drawing.Size(441, 40);
+            this.PasswordRecoveryTitle.Size = new System.Drawing.Size(662, 62);
             this.PasswordRecoveryTitle.TabIndex = 31;
             this.PasswordRecoveryTitle.Text = "Password Recovery";
             this.PasswordRecoveryTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -150,9 +160,10 @@
             this.PasswordRecoveryButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.PasswordRecoveryButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PasswordRecoveryButton.ForeColor = System.Drawing.SystemColors.Control;
-            this.PasswordRecoveryButton.Location = new System.Drawing.Point(284, 280);
+            this.PasswordRecoveryButton.Location = new System.Drawing.Point(426, 438);
+            this.PasswordRecoveryButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.PasswordRecoveryButton.Name = "PasswordRecoveryButton";
-            this.PasswordRecoveryButton.Size = new System.Drawing.Size(136, 34);
+            this.PasswordRecoveryButton.Size = new System.Drawing.Size(204, 53);
             this.PasswordRecoveryButton.TabIndex = 4;
             this.PasswordRecoveryButton.Text = "Recover";
             this.PasswordRecoveryButton.UseVisualStyleBackColor = false;
@@ -160,18 +171,20 @@
             // 
             // Password_Recovery
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
             this.BackgroundImage = global::Barbiere_WCF_Client.Properties.Resources.bg_dash;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(1133, 620);
+            this.ClientSize = new System.Drawing.Size(1700, 969);
             this.Controls.Add(this.Panel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "Password_Recovery";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Password Recovery";
+            this.Load += new System.EventHandler(this.Password_Recovery_Load);
             this.Panel.ResumeLayout(false);
             this.Panel.PerformLayout();
             this.ResumeLayout(false);
